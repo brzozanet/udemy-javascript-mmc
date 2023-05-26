b0ab035d279fd2702d23c310887a47af3ed15ed6
Paweł Brzozowski <pawel@brzoza.net>
2023-05-26 18:37:36 +0200

    Project 103

--- [11] projekty/103 Chowanie obrazka/script.js
+++ [11] projekty/103 Chowanie obrazka/script.js
@@ -2,14 +2,40 @@ const itemElement = document.querySelector(".item1");
 const buttonElement = document.querySelector(".arrow");
 const arrowElement = document.querySelector(".fas");
 
+// ====================== 1 ======================
+
+// const toggleImage = () => {
+//   itemElement.classList.toggle("hide");
+// };
+
+// const changeArrow = () => {
+//     arrowElement.classList.toggle("fa-arrow-up");
+//     arrowElement.classList.toggle("fa-arrow-down");
+// };
+
+// buttonElement.addEventListener("click", toggleImage);
+// buttonElement.addEventListener("click", changeArrow);
+
+// ====================== 2 ======================
+
+// const toggleImage = () => {
+//   itemElement.classList.toggle("hide");
+//   arrowElement.classList.toggle("fa-arrow-up");
+//   arrowElement.classList.toggle("fa-arrow-down");
+// };
+
+// buttonElement.addEventListener("click", toggleImage);
+
+// ====================== 3 ======================
+
 const toggleImage = () => {
   itemElement.classList.toggle("hide");
-};
 
-const changeArrow = () => {
-    arrowElement.classList.toggle("fa-arrow-up");
-    arrowElement.classList.toggle("fa-arrow-down");
+  if (itemElement.classList.contains("hide")) {
+    arrowElement.style.transform = "rotate(180deg)";
+  } else {
+    arrowElement.style.transform = "rotate(0deg)";
+  }
 };
 
 buttonElement.addEventListener("click", toggleImage);
-buttonElement.addEventListener("click", changeArrow);

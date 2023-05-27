654966cb73743f6900d0596b8e477eeb97098b7f
Paweł Brzozowski <pawel@brzoza.net>
2023-05-27 18:31:09 +0200

    Project 106

---
+++ [11] projekty/106 Konwerter/script.js
@@ -0,0 +1,10 @@
+// T(F) = T(C) * 1.8 + 32
+// T(C) = (T(F) - 32) / 1.8
+
+const converterEL = document.querySelector("#converter");
+const convBtnEl = document.querySelector(".conv");
+const resetBtnEl = document.querySelector(".reset");
+const changeBtnEl = document.querySelector(".change");
+const spanOneEl = document.querySelector(".one");
+const spanTwoEl = document.querySelector(".two");
+

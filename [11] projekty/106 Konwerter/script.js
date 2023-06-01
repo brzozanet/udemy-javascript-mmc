48fb1261d7a5ad17e6895f9e6133b764d84d736a
Paweł Brzozowski <pawel@brzoza.net>
2023-06-01 15:50:12 +0200

    Project 106

--- [11] projekty/106 Konwerter/script.js
+++ [11] projekty/106 Konwerter/script.js
@@ -1,17 +1,41 @@
 // T(F) = T(C) * 1.8 + 32
 // T(C) = (T(F) - 32) / 1.8
 
-const numberEL = document.querySelector("#number");
+const numberEl = document.querySelector("#number");
 const convBtnEl = document.querySelector(".conv");
 const resetBtnEl = document.querySelector(".reset");
 const changeBtnEl = document.querySelector(".change");
-const spanCelEl = document.querySelector(".stc");
-const spanFarEl = document.querySelector(".stf");
+const resultEl = document.querySelector(".result");
+const spanOneEl = document.querySelector(".one");
+const spanTwoEl = document.querySelector(".two");
 
-let result;
+let result = new Number();
 
 function reset() {
-    numberEL.value = "";
+  numberEl.value = "";
+  resultEl.innerHTML = "";
 }
 
+function changeUnit() {
+  if (spanOneEl.innerHTML === "°C") {
+    spanOneEl.innerHTML = "°F";
+    spanTwoEl.innerHTML = "°C";
+  } else {
+    spanOneEl.innerHTML = "°C";
+    spanTwoEl.innerHTML = "°F";
+  }
+}
+
+function convert() {
+  if (spanOneEl.innerHTML === "°C") {
+    result = numberEl.value * 1.8 + 32;
+    resultEl.innerHTML = result.toFixed(1);
+  } else {
+    result = (numberEl.value - 32) / 1.8;
+    resultEl.innerHTML = result.toFixed(1);
+  }
+}
+
+convBtnEl.addEventListener("click", convert);
 resetBtnEl.addEventListener("click", reset);
+changeBtnEl.addEventListener("click", changeUnit);
